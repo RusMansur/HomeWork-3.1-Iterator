d955f27c33c40d005518bcf1ccb47a5594291b12
RusMansur <rusdolg@gmail.com>
2022-04-19 17:01:10 +0300

    Первоначальная версия

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Randoms random = new Randoms(90, 100);
+        for (int r : random) {
+            System.out.println("Случайное число: " + r);
+            if (r == 100) {
+                System.out.println("Выпало число 100, давайте на этом закончим");
+                break;
+            }
+        }
+    }
+}
